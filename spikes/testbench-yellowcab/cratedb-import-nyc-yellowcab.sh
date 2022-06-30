d600bf26c4b2762e585c4057fc30da8973f41318
Andreas Motl <andreas.motl@crate.io>
2022-06-30 17:59:01 +0200

    NYC Yellowcab example: Improve SQL statement and output formatting

--- spikes/testbench-yellowcab/cratedb-import-nyc-yellowcab.sh
+++ spikes/testbench-yellowcab/cratedb-import-nyc-yellowcab.sh
@@ -17,14 +17,14 @@ CRATEDB_IMAGE=${CRATEDB_IMAGE:-crate:4.8.1}
 # 0. Define incantations.
 crash="docker run --interactive --rm --network=host ${CRATEDB_IMAGE} crash"
 cratedb_start="docker run --detach --rm --publish=4200:4200 --publish=5432:5432 --health-cmd=\"curl http://localhost:4200\" --health-interval=1s --health-start-period=5s --health-retries=15 --name=${CONTAINER_NAME} ${CRATEDB_IMAGE} -Cdiscovery.type=single-node"
-cratedb_stop="docker stop ${CONTAINER_NAME}"
+cratedb_stop="docker rm ${CONTAINER_NAME} --force"
 cratedb_status="docker inspect -f {{.State.Health.Status}} ${CONTAINER_NAME}"
 echo=$(which echo)
 
 # 1. Start CrateDB and wait for availability.
 $cratedb_status > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-  echo "Starting CrateDB."
+  echo "Starting CrateDB '${CRATEDB_IMAGE}'."
   sh -c "$cratedb_start"
 else
   echo "CrateDB already started."
@@ -37,11 +37,30 @@ done;
 echo
 
 # 2. Insert NYC Yellowcab data.
-echo "Importing NYC Yellowcab data. This will take a few seconds/minutes."
+echo "Importing NYC Yellowcab data."
+echo "This will take a few seconds or minutes, depending on system performance and network speed."
 time $crash <<EOF
 CREATE TABLE IF NOT EXISTS "nyc_taxi"
-  ("congestion_surcharge" REAL, "dolocationid" INTEGER, "extra" REAL, "fare_amount" REAL, "improvement_surcharge" REAL, "mta_tax" REAL, "passenger_count" INTEGER, "payment_type" INTEGER, "pickup_datetime" TIMESTAMP WITH TIME ZONE, "pulocationid" INTEGER, "ratecodeid" INTEGER, "store_and_fwd_flag" TEXT, "tip_amount" REAL, "tolls_amount" REAL, "total_amount" REAL, "trip_distance" REAL, "vendorid" INTEGER)
-  WITH ("column_policy" = 'dynamic', "number_of_replicas" = '0', "refresh_interval" = 10000);
+  (
+    "congestion_surcharge" REAL,
+    "dolocationid" INTEGER,
+    "extra" REAL,
+    "fare_amount" REAL,
+    "improvement_surcharge" REAL,
+    "mta_tax" REAL,
+    "passenger_count" INTEGER,
+    "payment_type" INTEGER,
+    "pickup_datetime" TIMESTAMP WITH TIME ZONE,
+    "pulocationid" INTEGER,
+    "ratecodeid" INTEGER,
+    "store_and_fwd_flag" TEXT,
+    "tip_amount" REAL,
+    "tolls_amount" REAL,
+    "total_amount" REAL,
+    "trip_distance" REAL,
+    "vendorid" INTEGER
+  )
+  WITH ("number_of_replicas" = '0', "refresh_interval" = 1000);
 
 COPY "nyc_taxi"
   FROM 'https://s3.amazonaws.com/crate.sampledata/nyc.yellowcab/yc.2019.07.gz'
@@ -60,9 +79,15 @@ echo
 
 # 4. User notes
 echo
-echo "The CrateDB database service has been started and populated with a subset of the NYC Yellowcab data into the table 'doc.nyc_taxi'."
-echo "The administration interface is available at http://localhost:4200."
-echo
-echo "If you are finished, you may want to shut down the database service using '${cratedb_stop}'."
-echo "Enjoy conducting your experiments."
-echo
+cat <<EOF
+The CrateDB database service has been started and populated with a
+subset of the NYC Yellowcab data into the table 'doc.nyc_taxi'.
+
+The CrateDB administration interface is available at http://localhost:4200.
+
+If you are finished, you may want to shut down and remove any remnants
+of the database service container using '${cratedb_stop}'.
+
+Enjoy conducting your experiments.
+
+EOF
