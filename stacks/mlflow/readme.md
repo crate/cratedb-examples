68bfceec975720306043666ecd4ccdcf4458086c
Andreas Motl <andreas.motl@crate.io>
2023-09-06 14:37:44 +0200

    MLflow: Add example program and guidelines

---
+++ stacks/mlflow/readme.md
@@ -0,0 +1,114 @@
+# MLflow and CrateDB
+
+
+## About MLflow
+
+[MLflow] is an open source platform to manage the ML lifecycle, including
+experimentation, reproducibility, deployment, and a central model registry.
+MLflow currently offers four components:
+
+- [MLflow Tracking]\: Record and query experiments: code, data, config, and results
+- [MLflow Projects]\: Package data science code in a format to reproduce runs on any platform
+- [MLflow Models]\: Deploy machine learning models in diverse serving environments
+- [MLflow Model Registry]\: Store, annotate, discover, and manage models in a central repository.
+
+An [MLflow Model][MLflow Models] is a standard format for packaging machine
+learning models that can be used in a variety of downstream tools—for example,
+real-time serving through a REST API or batch inference on Apache Spark. The
+format defines a convention that lets you save a model in different "flavors"
+that can be understood by different downstream tools.
+
+
+## What's inside
+
+This folder provides guidelines and runnable code to get started with [MLflow]
+and [CrateDB].
+
+- [readme.md](readme.md): The file you are currently reading contains a walkthrough
+  about how to install an MLflow stack, and how to use the [MLflow Tracking]
+  subsystem on behalf on an experiment run.
+- [requirements.txt](requirements.txt): Pulls in a patched version of MLflow,
+  [mlflow-cratedb], and the [Merlion] library. MLflow has been adjusted not to
+  invoke the database provisioning based on SQLAlchemy and Alembic, but uses a
+  traditional approach by running a bunch of SQL DDL statements, fitted to be
+  usable with CrateDB.
+- [tracking_merlion.py](tracking_merlion.py): The example program uses [Merlion],
+  a Python library for time series intelligence, which can be used for time series
+  anomaly detection and forecasting. Before running an experiment for demonstration
+  purposes, it will import the `machine_temperature_system_failure.csv` dataset
+  from the [Numenta Anomaly Benchmark] data.
+
+
+## Setup
+
+In order to spin up a CrateDB instance without further ado, you can use
+Docker or Podman.
+
+```shell
+docker run --rm -it --publish=4200:4200 --publish=5432:5432 \
+  --env=CRATE_HEAP_SIZE=4g crate \
+  -Cdiscovery.type=single-node \
+  -Ccluster.routing.allocation.disk.threshold_enabled=false
+```
+
+In order to properly setup a development sandbox environment, it is advised
+to create a Python virtualenv, and install the dependencies there. In this
+way, it is easy to wipe your virtualenv and start from scratch anytime.
+
+```shell
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Usage
+
+The upcoming commands expect that you are working on a terminal with
+activated virtualenv.
+```shell
+source .venv/bin/activate
+```
+
+### MLflow Server
+Start the MLflow server, pointing it towards your CrateDB instance.
+
+```shell
+mlflow server --backend-store-uri='crate://crate@localhost'
+```
+
+### Experiment
+In another terminal, run the Python program which defines the experiment. You can
+point it towards the MLflow server you just started, using the `MLFLOW_TRACKING_URI`
+environment variable, so the program will send events and outcomes from experiment
+runs, and the MLflow server will record them.
+
+```shell
+export MLFLOW_TRACKING_URI=http://127.0.0.1:5000
+python tracking_merlion.py
+```
+
+### Operations
+In order to run SQL DDL statements manually, you can use the `crash` program.
+```shell
+crash < mlflow/store/tracking/dbmodels/ddl/cratedb.sql
+crash < mlflow/store/tracking/dbmodels/ddl/drop.sql
+```
+
+You can also use `crash` to query for database records. Please note that the
+fork of MLflow adjusted for CrateDB stores its tables into the `mlflow` schema,
+so you will need to specify it when querying for data.
+
+```shell
+crash --command 'SELECT * FROM "mlflow"."experiments";'
+crash --command 'SELECT * FROM "mlflow"."runs";'
+```
+
+
+[CrateDB]: https://github.com/crate/crate
+[Merlion]: https://pypi.org/project/salesforce-merlion/
+[MLflow]: https://mlflow.org/
+[mlflow-cratedb]: https://github.com/crate-workbench/mlflow-cratedb
+[MLflow Models]: https://mlflow.org/docs/latest/models.html
+[MLflow Model Registry]: https://mlflow.org/docs/latest/model-registry.html
+[MLflow Projects]: https://mlflow.org/docs/latest/projects.html
+[MLflow Tracking]: https://mlflow.org/docs/latest/tracking.html
---
+++ stacks/mlflow/tracking_merlion.py
@@ -0,0 +1,170 @@
+"""
+Use MLflow to track the events and outcome of an ML experiment program using Merlion.
+"""
+import numpy as np
+import pandas as pd
+from crate import client
+from crate.client.exceptions import ProgrammingError
+from merlion.evaluate.anomaly import TSADMetric
+from merlion.models.defaults import DefaultDetector, DefaultDetectorConfig
+from merlion.utils import TimeSeries
+
+import mlflow
+
+
+def connect_database():
+    """
+    Connect to CrateDB, and return database connection object.
+    """
+    # TODO: Unlock for CrateDB Cloud.
+    #       https://github.com/crate-workbench/mlflow-cratedb/blob/07fa889362/mlflow/store/db/utils.py#L222-L236
+    # conn = client.connect("https://<your-instance>.azure.cratedb.net:4200",
+    #                       username="admin",
+    #                       password="<your-password>", verify_ssl_cert=True)
+    return client.connect("http://crate@localhost:4200")
+
+
+def provision_data():
+    """
+    Download Numenta Anomaly Benchmark data, and load into database.
+    """
+    data = pd.read_csv(
+        "https://raw.githubusercontent.com/numenta/NAB/master/data/realKnownCause/machine_temperature_system_failure.csv"
+    )
+
+    # Connecting to crate
+    conn = connect_database()
+    try:
+        conn.cursor().execute("SELECT * FROM machine_data LIMIT 1;")
+        return
+    except ProgrammingError as ex:
+        if "Relation 'machine_data' unknown" not in str(ex):
+            raise
+
+    # Split the data into chunks of 1000 rows each for better insert performance
+    chunk_size = 1000
+    chunks = np.array_split(data, int(len(data) / chunk_size))
+
+    # Insert the data into CrateDB
+    with conn:
+        cursor = conn.cursor()
+        # Create the table if it doesn't exist.
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS machine_data "
+            "(timestamp TIMESTAMP, temperature DOUBLE)"
+        )
+        # Insert the data in chunks.
+        for chunk in chunks:
+            cursor.executemany(
+                "INSERT INTO machine_data (timestamp, temperature) VALUES (?, ?)",
+                list(chunk.itertuples(index=False, name=None)),
+            )
+
+
+def read_data() -> pd.DataFrame:
+    """
+    Read data from database into pandas DataFrame.
+    """
+    conn = connect_database()
+    with conn:
+        cursor = conn.cursor()
+        cursor.execute(
+            """SELECT 
+         DATE_BIN('5 min'::INTERVAL, "timestamp", 0) AS timestamp,
+         max(temperature) as value
+         FROM machine_data
+         group by timestamp
+         order by timestamp asc"""
+        )
+        data = cursor.fetchall()
+
+    # Convert database response to pandas DataFrame.
+    time_series = pd.DataFrame(
+        [
+            {"timestamp": pd.Timestamp.fromtimestamp(item[0] / 1000), "value": item[1]}
+            for item in data
+        ]
+    )
+    # Set the timestamp as the index
+    time_series = time_series.set_index("timestamp")
+    return time_series
+
+
+def run_experiment(time_series: pd.DataFrame):
+    """
+    Run experiment on DataFrame, using Merlion. Track it using MLflow.
+    """
+    mlflow.set_experiment("numenta-merlion-experiment")
+
+    with mlflow.start_run():
+        train_data = TimeSeries.from_pd(
+            time_series[time_series.index < pd.to_datetime("2013-12-15")]
+        )
+        test_data = TimeSeries.from_pd(
+            time_series[time_series.index >= pd.to_datetime("2013-12-15")]
+        )
+
+        model = DefaultDetector(DefaultDetectorConfig())
+        model.train(train_data=train_data)
+
+        test_pred = model.get_anomaly_label(time_series=test_data)
+
+        # Prepare the test labels
+        time_frames = [
+            ["2013-12-15 17:50:00.000000", "2013-12-17 17:00:00.000000"],
+            ["2014-01-27 14:20:00.000000", "2014-01-29 13:30:00.000000"],
+            ["2014-02-07 14:55:00.000000", "2014-02-09 14:05:00.000000"],
+        ]
+
+        time_frames = [
+            [pd.to_datetime(start), pd.to_datetime(end)] for start, end in time_frames
+        ]
+        time_series["test_labels"] = 0
+        for start, end in time_frames:
+            time_series.loc[
+                (time_series.index >= start) & (time_series.index <= end), "test_labels"
+            ] = 1
+
+        test_labels = TimeSeries.from_pd(time_series["test_labels"])
+
+        p = TSADMetric.Precision.value(ground_truth=test_labels, predict=test_pred)
+        r = TSADMetric.Recall.value(ground_truth=test_labels, predict=test_pred)
+        f1 = TSADMetric.F1.value(ground_truth=test_labels, predict=test_pred)
+        mttd = TSADMetric.MeanTimeToDetect.value(
+            ground_truth=test_labels, predict=test_pred
+        )
+        print(
+            f"Precision: {p:.4f}, Recall: {r:.4f}, F1: {f1:.4f}\n"
+            f"Mean Time To Detect: {mttd}"
+        )
+
+        mlflow.log_metric("precision", p)
+        mlflow.log_metric("recall", r)
+        mlflow.log_metric("f1", f1)
+        mlflow.log_metric("mttd", mttd.total_seconds())
+        mlflow.log_param("anomaly_threshold", model.config.threshold.alm_threshold)
+        mlflow.log_param("min_alm_window", model.config.threshold.min_alm_in_window)
+        mlflow.log_param(
+            "alm_window_minutes", model.config.threshold.alm_window_minutes
+        )
+        mlflow.log_param(
+            "alm_suppress_minutes", model.config.threshold.alm_suppress_minutes
+        )
+        mlflow.log_param("ensemble_size", model.config.model.combiner.n_models)
+
+        # Save the model to mlflow
+        model.save("model")
+        mlflow.log_artifact("model")
+
+
+def main():
+    """
+    Provision dataset, and run experiment.
+    """
+    provision_data()
+    df = read_data()
+    run_experiment(df)
+
+
+if __name__ == "__main__":
+    main()
