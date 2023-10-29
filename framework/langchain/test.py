655ef138de8757aa15035d55506553dca3546129
Andreas Motl <andreas.motl@crate.io>
2023-10-29 23:44:39 +0100

    LangChain: Fix Jupyter Notebook `vector_search.ipynb`

--- framework/langchain/test.py
+++ framework/langchain/test.py
@@ -46,6 +46,17 @@ def str_list(things):
     return map(str, things)
 
 
+@pytest.fixture(scope="session", autouse=True)
+def nltk_init():
+    """
+    Initialize nltk upfront, so that it does not run stray output into Jupyter Notebooks.
+    """
+    download_items = ["averaged_perceptron_tagger", "punkt"]
+    import nltk
+    for item in download_items:
+        nltk.download(item)
+
+
 @pytest.fixture(scope="function", autouse=True)
 def db_init():
     """
