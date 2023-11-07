3281bcb17f0c41506aa3861d679f3cd21e6fcfc0
Andreas Motl <andreas.motl@crate.io>
2023-11-07 22:13:58 +0100

    Improve testing Jupyter Notebooks
    In order to improve the development experience when authoring Jupyter
    Notebooks, this patch pulls in generalized utility functions from
    other libraries, mostly `cratedb-toolkit` and `pueblo.testing`, so they
    don't need to be repeated as boilerplate code over and over again.
    
    Rename example `timeseries-basics` to `mlops-mlflow`.
    
    Add Jupyter Notebook about MLflow/Merlion to `mlops-mlflow` directory
    from `ml-sandbox` repository.

--- topic/machine-learning/llm-langchain/conftest.py
+++ topic/machine-learning/llm-langchain/conftest.py
@@ -1,4 +1,6 @@
-from pueblo.testing.notebook import monkeypatch_pytest_notebook_treat_cell_exit_as_notebook_skip
+# Initialize nltk upfront, so that it does not run stray output into Jupyter Notebooks.
+from pueblo.testing.nlp import nltk_init
 
 # Make `pytest.exit()` called in notebook cells gracefully skip testing the whole notebook.
+from pueblo.testing.notebook import monkeypatch_pytest_notebook_treat_cell_exit_as_notebook_skip
 monkeypatch_pytest_notebook_treat_cell_exit_as_notebook_skip()
