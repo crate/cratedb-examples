17990ca1034793f2c1e02f12494aedde0a30a3b1
Mario Ackerl <mario.ackerl@gmail.com>
2023-11-23 11:58:10 +0100

    Adding a working example with Crate DB and panache
    * introduce CrateDbDialect
    ** specializing on differences to Postgres 14
    ** no sequences
    ** different timestamp creation (no prescision)
    ** map JSON as OBJECT(DYNAMIC)
    ** map array as array(elementTypeName) instead of ... elementTypeName array
    ** exclude unsupported features
    * introduce CrateJsonType without casting (postgres default)
    * show it on an example panache entity

---
+++ by-language/java-quarkus-panache/src/main/java/io/crate/MyApplication.java
@@ -0,0 +1,38 @@
+package io.crate;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+@ApplicationScoped
+public class MyApplication {
+    private static final Logger log = LoggerFactory.getLogger(MyApplication.class);
+
+    void onStart(@Observes StartupEvent event) {
+        log.info("starting the crate demo application.");
+
+        MyEntity.populateWithData();
+
+        List<MyEntity> data = MyEntity.listAll();
+
+        log.info("Print populated data");
+
+        for (MyEntity entity: data) {
+            log.info(entity.toString());
+        }
+    }
+
+    void onStop(@Observes ShutdownEvent event) {
+        log.info("i have done my duty and may lay my head to rest.");
+    }
+
+    public static void main(String[] args) {
+        Quarkus.run(args);
+    }
+}
