8749932dfb57e813277c10538153ec83d6b48164
Andreas Motl <andreas.motl@crate.io>
2023-12-07 21:17:22 +0100

    JavaScript/TypeORM: Use CrateDB dialect

--- by-language/javascript-typeorm/src/data-source.ts
+++ by-language/javascript-typeorm/src/data-source.ts
@@ -3,7 +3,7 @@ import { DataSource } from "typeorm"
 import { User } from "./entity/User"
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
+    type: "cratedb-postgres",
     host: "localhost",
     port: 5432,
     username: "crate",
