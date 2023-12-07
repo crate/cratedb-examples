ce929effb0468bc50418dabf31c23bde92abb4e4
Andreas Motl <andreas.motl@crate.io>
2023-12-07 04:25:24 +0100

    JavaScript/TypeORM: Use `@PrimaryGeneratedColumn("uuid")`: UUID strategy

--- by-language/javascript-typeorm/src/entity/User.ts
+++ by-language/javascript-typeorm/src/entity/User.ts
@@ -3,7 +3,7 @@ import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"
 @Entity()
 export class User {
 
-    @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn("uuid")
     id: number
 
     @Column()
