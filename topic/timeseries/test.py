af3cf3a9934e79de132cdfcfdc0e498edba54752
Andreas Motl <andreas.motl@crate.io>
2024-03-07 18:24:28 +0100

    Time Series QA: Make Dask notebook self-contained and testable
    Include data acquisition from Kaggle.

--- topic/timeseries/test.py
+++ topic/timeseries/test.py
@@ -1,3 +1,6 @@
+import os
+from pathlib import Path
+
 import pytest
 from testbook import testbook
 
@@ -7,6 +10,14 @@ def test_notebook(notebook):
     Execute Jupyter Notebook, one test case per .ipynb file.
     """
     if notebook.name == "dask-weather-data-import.ipynb":
-        raise pytest.skip("Depends on DOWNLOAD_PATH/daily_weather.parquet")
+
+        # Skip Kaggle tests when having no authentication information.
+        kaggle_auth_exists = Path("~/.kaggle/kaggle.json").exists() or (
+                "KAGGLE_USERNAME" in os.environ and "KAGGLE_KEY" in os.environ
+        )
+        if not kaggle_auth_exists:
+            raise pytest.skip(f"Kaggle dataset can not be tested "
+                              f"without authentication: {notebook.name}")
+
     with testbook(notebook) as tb:
         tb.execute()
