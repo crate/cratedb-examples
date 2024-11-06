7c163f215646b9273b7a5a94f7b81b6114ab6fea
Andreas Motl <andreas.motl@crate.io>
2024-11-06 11:23:36 +0100

    ML/LlamaIndex: Add software tests and CI configuration (#707)
    * ML/LlamaIndex: Adjustments to make it work with non-Azure OpenAI
    
    * ML/LlamaIndex: Add software tests and CI configuration

---
+++ topic/machine-learning/llama-index/test.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+import pytest
+
+from cratedb_toolkit.io.sql import DatabaseAdapter
+from dotenv import load_dotenv
+
+HERE = Path(__file__).parent
+
+
+@pytest.fixture()
+def cratedb() -> DatabaseAdapter:
+    return DatabaseAdapter(dburi="crate://crate@localhost:4200")
+
+
+@pytest.fixture(scope="function", autouse=True)
+def init_database(cratedb):
+    """
+    Initialize database.
+    """
+    cratedb.run_sql("DROP TABLE IF EXISTS time_series_data;")
+    cratedb.run_sql((HERE / "init.sql").read_text())
+
+
+def test_main(cratedb, capsys):
+    """
+    Execute `main.py` and verify outcome.
+    """
+
+    # Load the standalone configuration also for software testing.
+    # On CI, `OPENAI_API_KEY` will need to be supplied externally.
+    load_dotenv("env.standalone")
+
+    # Invoke the workload, in-process.
+    from main import main
+    main()
+
+    # Verify the outcome.
+    out = capsys.readouterr().out
+    assert "Answer was: The average value for sensor 1 is approximately 17.03." in out
