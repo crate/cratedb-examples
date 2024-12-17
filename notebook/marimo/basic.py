26af45dbc86f79adece9be298fc007e67ca425dc
Andreas Motl <andreas.motl@crate.io>
2024-12-17 01:05:29 +0100

    Marimo: Output to stdout, to make it work as script

--- notebook/marimo/basic.py
+++ notebook/marimo/basic.py
@@ -5,51 +5,56 @@
 # /// script
 # requires-python = ">=3.10"
 # dependencies = [
-#     "duckdb==1.1.1",
+#     "duckdb",
 #     "marimo",
 #     "pandas==2.2.3",
 #     "sqlalchemy-cratedb",
-#     "vega-datasets==0.9.0",
+#     "sqlalchemy==2.0.36",
 # ]
 # ///
 
 import marimo
 
-__generated_with = "0.9.1"
+__generated_with = "0.10.2"
 app = marimo.App(width="medium")
 
 
 @app.cell
-def __():
+def _():
     import marimo as mo
     return (mo,)
 
 
 @app.cell(hide_code=True)
-def __(mo):
+def _(mo):
     mo.md(
         """
-        # Querying dataframes
+        # Querying CrateDB using dataframes
 
-        This notebook shows how to use SQL to query Python dataframes.
+        This notebook shows how to use Marimo and CrateDB, with Python dataframes and SQL.
 
-        First, we create a dataframe called `df`.
+        First, we create a dataframe called `df`, populated with content from CrateDB's
+        built-in `sys.summits` table.
         """
     )
     return
 
 
 @app.cell
-def __():
-    from vega_datasets import data
+def _():
+    import pandas as pd
+    import sqlalchemy as sa
 
-    df = data.iris()
-    df.head()
-    return data, df
+    engine = sa.create_engine("crate://")
+    df = pd.read_sql(sql="SELECT * FROM sys.summits ORDER BY height DESC", con=engine)
+
+    print(df)
+    df
+    return df, engine, pd, sa
 
 
 @app.cell(hide_code=True)
-def __(mo):
+def _(mo):
     mo.md(
         f"""
         Next, we **create a SQL cell**.
@@ -60,23 +65,25 @@ def __(mo):
         2. **Right-click** the {mo.icon("lucide:circle-plus")} button to the **left of a cell**, and choose `SQL`.
 
         In the SQL cell, you can query dataframes in your notebook as if they were tables — just reference them by name.
+        For querying a data frame using SQL, Marimo uses DuckDB.
         """
     )
     return
 
 
 @app.cell
-def __(df, mo):
+def _(df, mo):
     result = mo.sql(
         f"""
-        SELECT species, mean(petalLength) as meanPetalLength FROM df GROUP BY species ORDER BY meanPetalLength
-        """, output=False
+        SELECT * FROM df WHERE region LIKE 'Bernina%' ORDER BY height DESC
+        """,
+        output=False,
     )
     return (result,)
 
 
 @app.cell(hide_code=True)
-def __(mo):
+def _(mo):
     mo.md(
         r"""
         The query output is returned to Python as a dataframe (Polars if you have it installed, Pandas otherwise).
@@ -90,7 +97,8 @@ def __(mo):
 
 
 @app.cell
-def __(result):
+def _(result):
+    print(result)
     result
     return
 
