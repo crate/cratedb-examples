ce76c59a4bb8fe364849d7f0aaddcb6eaa352919
Andreas Motl <andreas.motl@crate.io>
2025-03-21 23:34:43 +0100

    Elixir: Add minimal example

---
+++ by-language/elixir-postgrex/mix.exs
@@ -0,0 +1,24 @@
+defmodule CrateDBExample.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :cratedb_elixir_example,
+      version: "0.0.0",
+      elixir: "~> 1.0",
+      deps: deps(),
+    ]
+  end
+
+  def application() do
+    []
+  end
+
+  defp deps() do
+    [
+      {:jason, "~> 1.4"},
+      {:postgrex, "~> 0.19.2"},
+    ]
+  end
+
+end
