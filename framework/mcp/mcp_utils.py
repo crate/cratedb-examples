c51619ffb33afbef03cec3bc8c792b6620500aad
Andreas Motl <andreas.motl@crate.io>
2025-04-03 23:17:11 +0200

    MCP: Add example about PG-MCP

--- framework/mcp/mcp_utils.py
+++ framework/mcp/mcp_utils.py
@@ -1,5 +1,7 @@
 import io
 import json
+import logging
+
 import mcp.types as types
 from typing import Any
 
@@ -9,12 +11,19 @@ from mcp import ClientSession, McpError
 from pydantic import AnyUrl
 
 
+logger = logging.getLogger(__name__)
+
+
 class McpDatabaseConversation:
     """
     Wrap database conversations through MCP servers.
     """
     def __init__(self, session: ClientSession):
         self.session = session
+
+    @staticmethod
+    def decode_json_text(thing):
+        return json.loads(thing.content[0].text)
 
     def decode_items(self, items):
         return list(map(self.decode_item, json.loads(pydantic_core.to_json(items))))
@@ -36,26 +45,38 @@ class McpDatabaseConversation:
             buffer.write("```\n")
         return buffer.getvalue()
 
+    async def entity_info(self, fun, attribute):
+        try:
+            return self.list_items(getattr(await fun(), attribute))
+        except McpError as e:
+            logger.error(f"Not implemented on this server: {e}")
+
+    @staticmethod
+    def dump_info(results):
+        if results:
+            print(results)
+        print()
+
     async def inquire(self):
         print("# MCP server inquiry")
         print()
 
         # List available prompts
         print("## Prompts")
-        try:
-            print(self.list_items((await self.session.list_prompts()).prompts))
-        except McpError as e:
-            print(f"Not implemented on this server: {e}")
+        self.dump_info(await self.entity_info(self.session.list_prompts, "prompts"))
+
+        # List available resources and resource templates
+        print("## Resources")
+        self.dump_info(await self.entity_info(self.session.list_resources, "resources"))
         print()
 
-        # List available resources
-        print("## Resources")
-        print(self.list_items((await self.session.list_resources()).resources))
+        print("## Resource templates")
+        self.dump_info(await self.entity_info(self.session.list_resource_templates, "resourceTemplates"))
         print()
 
         # List available tools
         print("## Tools")
-        print(self.list_items((await self.session.list_tools()).tools))
+        self.dump_info(await self.entity_info(self.session.list_tools, "tools"))
         print()
 
     async def call_tool(
