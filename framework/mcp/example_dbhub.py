c633c2f5f48609c0ea8e29e1d4d7a44f6fe7d58b
Andreas Motl <andreas.motl@crate.io>
2025-04-14 19:19:53 +0200

    MCP: Update to DBHub 0.3.1

--- framework/mcp/example_dbhub.py
+++ framework/mcp/example_dbhub.py
@@ -14,7 +14,7 @@ server_params_npx = StdioServerParameters(
     command="npx",
     args=[
         "-y",
-        "@bytebase/dbhub@0.2.3",
+        "@bytebase/dbhub@0.3.1",
         "--transport=stdio",
         #"--transport=sse",
         #"--port=8080",
