6f7cd572842f630da519ba05ef662ec283610785
Andreas Motl <andreas.motl@crate.io>
2025-07-20 04:10:45 +0200

    LlamaIndex: Make model configurable

--- topic/machine-learning/llama-index/boot.py
+++ topic/machine-learning/llama-index/boot.py
@@ -1,15 +1,20 @@
 import os
+from typing import Tuple
 
 import openai
 from langchain_openai import AzureOpenAIEmbeddings
 from langchain_openai import OpenAIEmbeddings
-from llama_index.core import Settings
+from llama_index.core.base.embeddings.base import BaseEmbedding
+from llama_index.core.llms import LLM
 from llama_index.llms.azure_openai import AzureOpenAI
 from llama_index.llms.openai import OpenAI
 from llama_index.embeddings.langchain import LangchainEmbedding
 
 
-def configure_llm():
+MODEL_NAME = "gpt-4o"
+
+
+def configure_llm() -> Tuple[LLM, BaseEmbedding]:
     """
     Configure LLM. Use either vanilla Open AI, or Azure Open AI.
     """
@@ -21,27 +26,32 @@ def configure_llm():
 
     if openai.api_type == "openai":
         llm = OpenAI(
+            model=MODEL_NAME,
+            temperature=0.0,
             api_key=os.getenv("OPENAI_API_KEY"),
-            temperature=0.0
         )
     elif openai.api_type == "azure":
         llm = AzureOpenAI(
+            model=MODEL_NAME,
+            temperature=0.0,
             engine=os.getenv("LLM_INSTANCE"),
             azure_endpoint=os.getenv("OPENAI_AZURE_ENDPOINT"),
             api_key = os.getenv("OPENAI_API_KEY"),
             api_version = os.getenv("OPENAI_AZURE_API_VERSION"),
-            temperature=0.0
         )
     else:
         raise ValueError(f"Open AI API type not defined or invalid: {openai.api_type}")
 
-    Settings.llm = llm
     if openai.api_type == "openai":
-        Settings.embed_model = LangchainEmbedding(OpenAIEmbeddings())
+        embed_model = LangchainEmbedding(OpenAIEmbeddings(model=MODEL_NAME))
     elif openai.api_type == "azure":
-        Settings.embed_model = LangchainEmbedding(
+        embed_model = LangchainEmbedding(
             AzureOpenAIEmbeddings(
                 azure_endpoint=os.getenv("OPENAI_AZURE_ENDPOINT"),
                 model=os.getenv("EMBEDDING_MODEL_INSTANCE")
             )
         )
+    else:
+        embed_model = None
+
+    return llm, embed_model
