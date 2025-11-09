c548a4c58d1d6e531fd61b9ccc5795e528ac613f
Andreas Motl <andreas.motl@crate.io>
2025-11-09 21:18:24 +0100

    Testcontainers/Java/JUnit 5: Add missing support. Disambiguate docs.

--- testing/testcontainers/java/src/test/java/io/crate/example/testing/TestClassScope.java
+++ testing/testcontainers/java/src/test/java/io/crate/example/testing/TestClassScope.java
@@ -3,6 +3,8 @@ package io.crate.example.testing;
 import io.crate.example.testing.utils.TestingHelpers;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.cratedb.CrateDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -11,19 +13,20 @@ import static io.crate.example.testing.utils.TestingHelpers.assertResults;
 
 
 /**
- * Class-scoped testcontainer instance with JUnit 4 @Rule/@ClassRule integration.
+ * Class-scoped testcontainer instance with JUnit 5.
  * <p>
- * In case you can't use the URL support, or need to fine-tune the container, you can instantiate it yourself.
- * Note that if you use @Rule, you will be given an isolated container for each test method.
- * If you use @ClassRule, you will get on isolated container for all the methods in the test class.
+ * The extension finds all fields that are annotated with @Container and calls their container
+ * lifecycle methods (methods on the Startable interface).
+ * Containers declared as static fields will be shared between test methods.
  * </p>
  * <p>
- *   <a href="https://www.testcontainers.org/modules/databases/jdbc/#database-container-objects"/>
- *   <a href="https://www.testcontainers.org/test_framework_integration/junit_4/#ruleclassrule-integration"/>
+ *   <a href="https://java.testcontainers.org/test_framework_integration/junit_5/#shared-containers"/>
  * </p>
  */
+@Testcontainers
 public class TestClassScope {
 
+    @Container
     public static CrateDBContainer cratedb = new CrateDBContainer(TestingHelpers.nameFromLabel("5.10"));
 
     @Test
