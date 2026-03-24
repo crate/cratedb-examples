a1319107ce1d73a84bf08bf0df0a6b48fa47f37e
Andreas Motl <andreas.motl@crate.io>
2026-03-24 02:08:03 +0100

    pgx: Start validating PostgreSQL protocol version 3.2 (PostgreSQL 18)

--- by-language/go-pgx/main.go
+++ by-language/go-pgx/main.go
@@ -8,8 +8,11 @@ import (
 func main() {
 	hosts := flag.String("hosts", "localhost", "CrateDB hostname")
 	port := flag.Int("port", 5432, "CrateDB postgres port")
+	protocol_version := flag.String("protocol-version", "3.0", "PostgreSQL protocol version")
 	flag.Parse()
-	connStr := fmt.Sprintf("postgres://crate@%s:%d/testdrive", *hosts, *port)
+	connStr := fmt.Sprintf(
+	    "postgres://crate@%s:%d/testdrive?min_protocol_version=%s&max_protocol_version=%s",
+	    *hosts, *port, *protocol_version, *protocol_version)
 	runBasicQueries(connStr)
 	runBulkOperations(connStr)
 }
