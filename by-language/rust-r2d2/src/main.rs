8ebaf1eec48c587154b6678ff6a8e1b881f25ab8
Andreas Motl <andreas.motl@crate.io>
2026-04-07 20:46:58 +0200

    Rust: Add `rust-r2d2` exercise

---
+++ by-language/rust-r2d2/src/main.rs
@@ -0,0 +1,38 @@
+use std::time::Duration;
+
+use postgres::{NoTls, Row};
+use r2d2_postgres::{
+    r2d2::{Pool},
+    PostgresConnectionManager,
+};
+
+mod tests;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    // Connect to database.
+    let pg_manager = PostgresConnectionManager::new(
+        "postgresql://crate:crate@localhost:5432/?sslmode=disable"
+            .parse()
+            .unwrap(),
+        NoTls,
+    );
+    let pg_pool = Pool::builder()
+        .max_size(5)
+        .connection_timeout(Duration::from_secs(2))
+        .build(pg_manager)
+        .expect("Postgres pool failed");
+    let mut pg_conn = pg_pool.get().unwrap();
+
+    // Invoke query.
+    let result = pg_conn.query("SELECT mountain, height FROM sys.summits ORDER BY height DESC LIMIT 3", &[]);
+    let rows = result.unwrap().into_iter().collect::<Vec<Row>>();
+
+    // Display results.
+    for row in rows {
+        let mountain: &str = row.get(0);
+        let height: i32 = row.get(1);
+        println!("{}: {}", mountain, height);
+    }
+    Ok(())
+}
