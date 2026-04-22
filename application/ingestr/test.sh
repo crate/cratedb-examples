c267cad9941232a099c986af90518f0a3983d9c5
Andreas Motl <andreas.motl@elmyra.de>
2026-04-22 09:38:16 +0200

    Update application/ingestr/test.sh

--- application/ingestr/test.sh
+++ application/ingestr/test.sh
@@ -18,8 +18,6 @@ function setup() {
 
 # Invoke Kafka tests.
 function test_kafka() {
-  # Use specific version of Xonsh until #6354 is resolved.
-  # https://github.com/xonsh/xonsh/issues/6354
   uvx 'xonsh==0.23.1' kafka-demo.xsh
 }
 
