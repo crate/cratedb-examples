cae8b551ab8729e8c2669e8d8f38568309660ffc
Andy Kipp <anki-code@users.noreply.github.com>
2026-04-22 13:38:40 +0600

    ingestr: Use Xonsh 0.23.1 (#1634)
    * Update Xonsh version in Kafka test function

--- application/ingestr/test.sh
+++ application/ingestr/test.sh
@@ -18,9 +18,7 @@ function setup() {
 
 # Invoke Kafka tests.
 function test_kafka() {
-  # Use specific version of Xonsh until #6354 is resolved.
-  # https://github.com/xonsh/xonsh/issues/6354
-  uvx 'xonsh==0.22.*' kafka-demo.xsh
+  uvx 'xonsh==0.23.1' kafka-demo.xsh
 }
 
 # Invoke Elasticsearch tests.
